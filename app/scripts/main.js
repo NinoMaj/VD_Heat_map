f22954f9c6780fca85b50454c5f3d60546f8465f
Nino Majder <Nino Majder>
2017-01-02 22:14:44 +0100

    First commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,37 +1,38 @@
 let request = new XMLHttpRequest();
-request.open('GET', 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json', true); // The last argument specifies whether you want your request to run asynchronously or not.
+request.open('GET', 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/global-temperature.json', true); // The last argument specifies whether you want your request to run asynchronously or not.
 request.send();
 request.onreadystatechange = () => {
     if (request.readyState == 4 && request.status == 200) {
         let response = JSON.parse(request.responseText);
-        console.log('resonse:', response);
-        let data = response;
+        let baseTemperature = response.baseTemperature;
+        console.log('baseTemperature:', baseTemperature);
+        let data = response.monthlyVariance;
         console.log('data:', data);
-        let fastestTime = data[0].Seconds;
-        console.log('fastestTime:', fastestTime);
 
-        // function addFlag(country) {
-        //     switch (country):
-        //         case 1:
-        //     }
-
+        data.forEach(function (d) {
+                d.date = new Date(d.year, d.month - 1);
+                d.month = +d.month;
+                d.year = +d.year;
+            });
+        
         // Set the dimensions of the canvas / graph
         let margin = { top: 30, right: 20, bottom: 40, left: 40 },
             width = 820 - margin.left - margin.right,
             height = 500 - margin.top - margin.bottom;
 
         function drawGraph() {
-            // Parse the date / time
-            let parseDate = d3.timeParse('%d-%b-%y');
-            let formatTime = d3.timeFormat("%S s");
+            // Format time
+            let formatTime = d3.timeFormat('%B, %Y');
 
             // Set the ranges
             let x = d3.scaleLinear().range([0, width]);
             let y = d3.scaleLinear().range([height, 0]);
+            let c = d3.scaleLinear().rangeRound([0, 12])
+            
 
             // Define the axes
-            let xAxis = d3.axisBottom(x).ticks(10);
-            let yAxis = d3.axisLeft(y).ticks(6);
+            let xAxis = d3.axisBottom(x).ticks();
+            let yAxis = d3.axisLeft(y); //.tickFormat(d3.timeFormat("%b")); // .tickValues(["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]);
 
             // Define the div for the tooltip
             let div = d3.select('body').append('div')
@@ -48,99 +49,48 @@ request.onreadystatechange = () => {
                 'translate(' + margin.left + ',' + margin.top + ')');
 
             // Scale the range of the data
-            x.domain([0, d3.max(data, function (d) { return d.Seconds - fastestTime; })]);
-            y.domain([0, d3.max(data, function (d) { return d.Place; })]);
+            x.domain(d3.extent(data, function (d) { return d.year;}));
+            y.domain([0, d3.max(data, function (d) { return d.month;})]);
+            c.domain(d3.extent(data, function (d) { return d.variance;}));
 
-            var defs = svg.append('svg:defs');
-
-            function putFlag(Nationality) {
-                let country = "";
-                switch (Nationality) {
-                    case "ITA":
-                        country = "IT"
-                        break;
-                    case "USA":
-                        country = "US"
-                        break;
-                    case "GER":
-                        country = "DE"
-                        break;
-                    case "GER":
-                        country = "DE"
-                        break;
-                    case "ESP":
-                        country = "ES"
-                        break;
-                    case "SUI":
-                        country = "CH"
-                        break;
-                    case "DEN":
-                        country = "DK"
-                        break;
-                    case "FRA":
-                        country = "FR"
-                        break;
-                    case "POR":
-                        country = "PT"
-                        break;
-                    case "COL":
-                        country = "CO"
-                        break;
-                    case "UKR":
-                        country = "UA"
-                        break;
-                    case "RUS":
-                        country = "RU"
-                        break;
-                    default:
-                        country = "HR";
-                }
-                defs.append("svg:pattern")
-                    .attr("id", country)
-                    .attr("width", 20)
-                    .attr("height", 20)
-                    .attr("patternUnits", "objectBoundingBox")
-                    .append("svg:image")
-                    .attr("xlink:href", `../images/png/${country}.png`)
-                    .attr("width", 20)
-                    .attr("height", 20)
-                    .attr("x", 0)
-                    .attr("y", -4);
-                return `url(#${country})`;
+            const colors = {
+                "0": "rgb(15, 240, 0)",
+                "1": "rgb(55, 240, 0)",
+                "2": "rgb(95, 240, 0)",
+                "3": "rgb(135, 240, 0)",
+                "4": "rgb(175, 240, 0)",
+                "5": "rgb(215, 240, 0)",
+                "6": "rgb(255, 240, 0)",
+                "7": "rgb(255, 200, 0)",
+                "8": "rgb(255, 160, 0)",
+                "9": "rgb(255, 120, 0)",
+                "10": "rgb(255, 80, 0)",
+                "11": "rgb(255, 40, 0)",
+                "12": "rgb(255, 0, 0)",
+            };
+            function getColor(variance) {
+              return colors[c(variance)];
             }
-
-            function strokeColor(doping) {
-                if (doping === "") {
-                    return "#33ff33";
-                } else {
-                    return "black";
-                }
-            }
+            
             // Add the scatterplot
             svg.selectAll('dot')
                 .data(data)
                 .enter().append('rect')
-                .attr('height', 13)
-                .attr('width', 20)
-                .style("fill", function (d) { return putFlag(d.Nationality) })
-                .style("stroke-width", "2")
-                .style("stroke", function (d) { return strokeColor(d.Doping) })
-                .attr('x', function (d) { return x(d.Seconds - fastestTime); })
-                .attr('y', function (d) { return y(d.Place) - 2; })
+                .attr('height', height / 12)
+                .attr('width', width / (2015 - 1753))
+                .style('fill', function (d) { return getColor(d.variance) })
+                .attr('x', function (d) { return x(d.year); })
+                .attr('y', function (d) { return y(d.month); })
                 .on('mouseover', function (d) {
                     div.transition()
                         .duration(200)
                         .style('opacity', .9);
                     div.html(
-                        `<b>Name: </b>  ${d.Name}</br>
-                        <b>Nationality: </b> ${d.Nationality}</br>
-                        <b>Place: </b> ${d.Place}</br>
-                        <b>Time: </b> ${d.Time}</br>
-                        <b>Year: </b> ${d.Year}</br>
-                        ${d.Doping}`
+                        `<b>Date: </b>  ${formatTime(d.date)}</br>
+                        <b>Diff from baseline: </b> ${d.variance}</br>`
                     )
-                        .style('left', (d3.event.pageX - 150) + 'px')
-                        .style('top', (d3.event.pageY - 150) + 'px');
+                        .style('left', (d3.event.pageX - 130) + 'px')
+                        .style('top', (d3.event.pageY - 60) + 'px');
                 })
                 .on('mouseout', function (d) {
                     div.transition()
@@ -151,57 +101,69 @@ request.onreadystatechange = () => {
             svg.append('g')
                 .attr('class', 'x axis')
                 .attr('transform', 'translate(0,' + height + ')')
+                .style('stroke-width', '0.5')
+                .style('stroke', 'white')
+                .style('font-size', '12')
                 .call(xAxis);
 
             // Add the Y Axis
             svg.append('g')
                 .attr('class', 'y axis')
+                .style('stroke-width', '0.5')
+                .style('stroke', 'white')
+                .style('font-size', '12')
                 .call(yAxis);
 
             // adding names to axes
-            svg.append("text")
-                .attr("text-anchor", "middle")  // this makes it easy to centre the text as the transform is applied to the anchor
-                .attr("transform", "translate(" + (-margin.left / 2 - 10) + "," + (30) + ")rotate(-90)")  // text is drawn off the screen top left, move down and out and rotate
-                .text("Ranking");
+            svg.append('text')
+                .attr('text-anchor', 'middle')  // this makes it easy to centre the text as the transform is applied to the anchor
+                .attr('transform', 'translate(' + (-margin.left / 2 - 8) + ',' + (20) + ')rotate(-90)')  // text is drawn off the screen top left, move down and out and rotate
+                .style('fill', 'white')
+                .style('font-size', '16')
+                .text('Month');
 
-            svg.append("text")
-                .attr("text-anchor", "middle")  // this makes it easy to centre the text as the transform is applied to the anchor
-                .attr("transform", "translate(" + (width - 50) + "," + (height + (margin.left / 2 + 10)) + ")")
-                .text("Seconds behind best time ever");
+            svg.append('text')
+                .attr('text-anchor', 'middle')  // this makes it easy to centre the text as the transform is applied to the anchor
+                .attr('transform', 'translate(' + (width - 20) + ',' + (height + (margin.left / 2 + 15)) + ')')
+                .style('fill', 'white')
+                .style('font-size', '16')
+                .text('Year');
 
-            svg.append("rect")
-                .attr('height', 13)
-                .attr('width', 20)
-                .style("fill", "none")
-                .style("stroke-width", "2")
-                .style("stroke", "#33ff33")
-                .attr("transform", "translate(" + (width - 160) + "," + (height - 52) + ")")
+            // svg.append('rect')
+            //     .attr('height', 13)
+            //     .attr('width', 20)
+            //     .style('fill', 'none')
+            //     .style('stroke-width', '2')
+            //     .style('stroke', '#33ff33')
+            //     .attr('transform', 'translate(' + (width - 160) + ',' + (height - 52) + ')')
 
-            svg.append("text")
-                .attr("text-anchor", "middle")  // this makes it easy to centre the text as the transform is applied to the anchor
-                .attr("transform", "translate(" + (width - 60) + "," + (height - 40) + ")")
-                .attr("font-weight", "bold")
-                .text("No doping alligations");
+            // svg.append('text')
+            //     .attr('text-anchor', 'middle')  // this makes it easy to centre the text as the transform is applied to the anchor
+            //     .attr('transform', 'translate(' + (width - 60) + ',' + (height - 40) + ')')
+            //     .attr('font-weight', 'bold')
+            //     .text('Warmer color ');
 
-            svg.append("rect")
-                .attr('height', 13)
-                .attr('width', 20)
-                .style("fill", "none")
-                .style("stroke-width", "2")
-                .style("stroke", "black")
-                .attr("transform", "translate(" + (width - 160) + "," + (height - 32) + ")")
+            // svg.append('rect')
+            //     .attr('height', 13)
+            //     .attr('width', 20)
+            //     .style('fill', 'none')
+            //     .style('stroke-width', '2')
+            //     .style('stroke', 'black')
+            //     .attr('transform', 'translate(' + (width - 160) + ',' + (height - 32) + ')')
 
-            svg.append("text")
-                .attr("text-anchor", "middle")  // this makes it easy to centre the text as the transform is applied to the anchor
-                .attr("transform", "translate(" + (width - 70) + "," + (height - 20) + ")")
-                .attr("font-weight", "bold")
-                .text("Doping alligations");
+            // svg.append('text')
+            //     .attr('text-anchor', 'middle')  // this makes it easy to centre the text as the transform is applied to the anchor
+            //     .attr('transform', 'translate(' + (width - 70) + ',' + (height - 20) + ')')
+            //     .attr('font-weight', 'bold')
+            //     .text('Doping alligations');
         }
         function show() {
-            console.log("button show pressed");
+            console.log('button show pressed');
             drawGraph();
-            document.querySelector('#source').style.visibility = "visible";
-            document.querySelector('.btn-show').style.display = "none";
+            document.querySelector('.btn-show').style.display = 'none';
+            document.querySelector('#source').style.visibility = 'visible';
+            document.querySelector('.graphExplinationText').style.visibility = 'visible';
+            document.querySelector('.graphExplinationText2').style.visibility = 'visible';
         }
         document.querySelector('.btn-show').addEventListener('click', show);
     }
